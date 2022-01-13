f1072e0a2ffa218bb412e0573826cf8bd281879b
jeantyg13 <jeantyg13@students.ecu.edu>
2022-01-13 16:45:09 -0500

    updated some inserts for sql db

--- server/sql/attack-prod.sql
+++ server/sql/attack-prod.sql
@@ -2,12 +2,12 @@ drop database if exists attack_prod;
 create database attack_prod;
 use attack_prod;
 
-
 create table users( 
 	user_id int primary key auto_increment,
     user_name varchar(50) not null unique,
     password varchar(50) not null,
-    high_score int 
+    high_score int,
+	disabled boolean not null default(0)
 );
 
 create table roles (
@@ -25,15 +25,12 @@ create table user_roles (
    
 );
 
-
-
 create table high_scores (
 	high_score_id int primary key auto_increment,
     high_score int not null,
     user_id int not null,
     
     constraint fk_userhighscore foreign key (user_id) references users(user_id)
-    
 );
 
 create table games (
@@ -41,3 +38,39 @@ create table games (
     game_name varchar(50) not null unique,
     game_description varchar(5000) not null
 );
+
+insert into games 
+	(game_name, game_description) 
+values
+	('Bay Window', 'The Bay Window task has you searching for the picture within the ring. Use your mouse to drag to spin the ring and find the image.'),
+	('Flashlight', 'The Flashlight tests your skills in finding an object while your field of view is very limited. Use your mouse to search through the darkness and find the light switch. Once you have found it flip the switch by clicking on it.'),
+	('Drag Counter', 'The Drag counter is a speed trial in recognizing the goal. Use your mouse to click and drag the counter to the required count. Once you have successfully done that click continue to move on.'),
+	('Bookshelf', 'The Bookshelf requires you to find a specific book in the bookshelf. Use your mouse to search for the requested book. Once you have found it click to confirm.'),
+	('Picture Tear','The Picture Tear is an investigation of wits to test your puzzle solving skills. Deduce the solution and enact the goal with your mouse to move on.');
+      
+-- passwords are set to "P@ssw0rd!"
+insert into users (user_name, password, high_score, disabled) values
+	('victory','$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa',33,0),
+	('carl','$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa',25,0),
+	('matthew','$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa',23,0),
+	('gatekeeper','$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa',456,0),
+	('captain','$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa',85,0);
+
+insert into roles (role_name) values
+    ('USER'),
+    ('ADMIN');
+    
+insert into user_roles values
+    (1, 2),
+    (2, 2),
+    (3, 2),
+    (4, 1),
+	(5, 1);
+    
+-- TODO
+-- insert into high_scores values (high_score)
+--     (33, 1),
+--     (25, 2),
+--     (23, 3),
+--     (456, 4),
+-- 	(85, 5);    
