2a7f784787d610b95de7706c54b571c474d9388d
jeantyg13 <jeantyg13@students.ecu.edu>
2022-01-17 15:27:58 -0500

    login update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import BayWindow from './games/windows/index.jsx';
 import FlashlightReact from './games/flashlight/flashlightReact';
-import Navbar from './components/navbar/Navbar';
 import DragCounter from './games/drag_counter/index.js';
 import Bookshelf from './games/bookshelf';
 import Login from './components/login/Login';
@@ -10,17 +9,45 @@ import Picture from './games/picture_tear/index'
 import Homepage from './components/homepage/Homepage';
 import Descriptions from './components/descriptions/Descriptions';
 import HighScores from './components/highScores/HighScores';
+import jwtDecode from 'jwt-decode';
+import {useEffect, useState} from 'react';
+import UserContext from './components/UserContext'
 
 import SignUp from './components/signup/SignUp';
 
 
 function App() {
 
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const userObject = {
+    currentUser,
+    setCurrentUser
+  };
+
+  const onLogout = () => {
+    localStorage.removeItem("jwt_token");
+    setCurrentUser(null);
+  }
+  useEffect(() => {
+
+
+    const existingJwt = localStorage.getItem("jwt_token");
+    if (existingJwt) {
+      const userObject = jwtDecode(existingJwt);
+
+      if (currentUser == null || userObject.sub !== currentUser.sub) {
+        setCurrentUser(userObject);
+        console.log(currentUser.status);
+      }
+    }
+  });
 
   return (
     <div className="App">
 
       <BrowserRouter>
+      <UserContext.Provider value={userObject}>
           {/* <Navbar /> */}
           <Switch>
             <Route exact path="/">
@@ -53,7 +80,11 @@ function App() {
             <Route path="/signup">
               <SignUp />
             </Route>
+            {/* <Route exact path="/users">
+            {currentUser ? <Users /> : <Redirect to="/"/>}
+          </Route> */}
           </Switch>
+          </UserContext.Provider>
       </BrowserRouter>
     </div>
   );
