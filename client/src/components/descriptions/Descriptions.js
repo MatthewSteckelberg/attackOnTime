063f8e32a9336511253160b01b2af9183df8c5ba
carl-33 <koepkecj@gmail.com>
2022-01-19 23:24:43 -0600

    added images to the descriptions

--- client/src/components/descriptions/Descriptions.js
+++ client/src/components/descriptions/Descriptions.js
@@ -1,30 +1,45 @@
 import { useEffect, useState } from 'react';
 import './descriptionsStyle.css';
 import Navbar from '../navbar/Navbar';
+import game1 from './images/game-1.png';
+import game2 from './images/game-2.png';
+import game3 from './images/game-3.png';
+import game4 from './images/game-4.png';
+import game5 from './images/game-5.png';
+
+
+
 
 function Descriptions(userObject) {
     const [gamelist, setgamelist] = useState([]);
 
     const getAllGames = () => {
         return fetch("http://localhost:8080/api/games")
-        .then((response) => response.json())
-        .then((data) => setgamelist(data));
+            .then((response) => response.json())
+            .then((data) => setgamelist(data));
     }
 
     useEffect(() => {
         getAllGames();
-    },[]);
+    }, []);
 
-
+let images = ['', game1, game2, game3, game4, game5]
 
 
     return (
         <div className='descriptions'>
-            <Navbar userObject={userObject}/>
-            {gamelist.map((games) => (<div key={games.gameId}><h2 className='description-header'>{games.name}</h2> 
-            <p className='game-description'>{games.description}</p></div> ))}    
+            <Navbar userObject={userObject} />
+            {gamelist.map((games) => (
+                <div key={games.gameId}>
+                    <h2 className='description-header'>{games.name}</h2>
+                    <div className='row'>
+                        <img src={images[games.gameId]} className='game-images col-lg-3 col-md-3 col-sm-8'/>
+                        <p className='game-description col-lg-8 col-md-8 col-sm-10'>{games.description}</p>
+                    </div>
+                </div>)
+            )}
 
-{/* 
+            {/* 
             <h2 className='description-header'>Bay Window</h2>
             <p className='game-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?</p>
             <h2 className='description-header'>Flashlight</h2>
